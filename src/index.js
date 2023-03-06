a542895cdd65c71214fba42ee0a47591ed0635f3
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2023-03-06 21:01:40 +0900

    Refactor getComponent: async func

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import _ from "lodash";
 
-function getComponent() {
-  return import("lodash")
-    .then(({ default: _ }) => {
-      const element = document.createElement("div");
+async function getComponent() {
+  const element = document.createElement("div");
+  const { default: _ } = await import("lodash");
 
-      element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-      return element;
-    })
-    .catch((error) => "An error occurred while loading the component");
+  return element;
 }
 
 getComponent().then((component) => {
